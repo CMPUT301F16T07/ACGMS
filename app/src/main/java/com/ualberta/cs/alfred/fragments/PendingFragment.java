31afa950d3d04174140a0a10a594ea2a26f1c5f3
ccastell <ccastell@ualberta.ca>
2016-11-09 12:17:25 -0700

    Saving

--- app/src/main/java/com/ualberta/cs/alfred/fragments/PendingFragment.java
+++ app/src/main/java/com/ualberta/cs/alfred/fragments/PendingFragment.java
@@ -3,7 +3,6 @@ package com.ualberta.cs.alfred.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,27 +10,25 @@ import android.view.ViewGroup;
 import com.ualberta.cs.alfred.R;
 
 /**
- * Created by carlcastello on 08/11/16.
+ * Created by carlcastello on 09/11/16.
  */
 
-public class ListFragment extends Fragment {
-
-    public ListFragment() {
+public class PendingFragment extends Fragment {
+    public PendingFragment() {
     }
 
-    public static ListFragment newInstance() {
+    public static PendingFragment newInstance() {
         Bundle args = new Bundle();
-        ListFragment listFragment = new ListFragment();
-        listFragment.setArguments(args);
-        return listFragment;
+        PendingFragment pendingFragment = new PendingFragment();
+        pendingFragment.setArguments(args);
+        return pendingFragment;
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_list,container,false);
+        View view = inflater.inflate(R.layout.fragment_pending,container,false);
         return view;
     }
-    
 
 }
