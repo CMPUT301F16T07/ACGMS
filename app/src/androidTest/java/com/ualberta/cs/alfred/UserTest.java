c4d43a62928c3bef7951399dbd25ac2ea3bf332a
ookmm <gaylordmbuyi@gmail.com>
2016-11-10 10:35:58 -0700

    Finished all getters in UserTest

--- app/src/androidTest/java/com/ualberta/cs/alfred/UserTest.java
+++ app/src/androidTest/java/com/ualberta/cs/alfred/UserTest.java
@@ -7,13 +7,25 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 
+/**
+ * Creates the test cases for type User.
+ *
+ * @author ookmm
+ * @version 1.0
+ */
 public class UserTest extends ActivityInstrumentationTestCase2 {
 
+    /**
+     * Instantiates a new User test.
+     */
     public UserTest() {
 
         super(MainActivity.class);
     }
 
+    /**
+     * Test get first name.
+     */
     public void testGetFirstName() {
 
         String u1FirstName = "Donald";
@@ -32,6 +44,9 @@ public class UserTest extends ActivityInstrumentationTestCase2 {
         assertTrue("Firstname not the same", u1FirstName.equals(u1.getFirstName()));
     }
 
+    /**
+     * Test get last name.
+     */
     public void testGetLastName() {
 
         String u1FirstName = "Donald";
@@ -50,6 +65,9 @@ public class UserTest extends ActivityInstrumentationTestCase2 {
         assertTrue("Lastname not the same", u1LastName.equals(u1.getLastName()));
     }
 
+    /**
+     * Test get user name.
+     */
     public void testGetUserName() {
 
         String u1FirstName = "Donald";
@@ -68,6 +86,9 @@ public class UserTest extends ActivityInstrumentationTestCase2 {
         assertTrue("Username not the same", u1UserName.equals(u1.getUserName()));
     }
 
+    /**
+     * Test get date of birth.
+     */
     public void testGetDateOfBirth() {
 
         String u1FirstName = "Donald";
@@ -86,6 +107,9 @@ public class UserTest extends ActivityInstrumentationTestCase2 {
         assertTrue("Date of birth not the same", u1DateOfBirth.equals(u1.getDateOfBirth()));
     }
 
+    /**
+     * Test get phone number.
+     */
     public void testGetPhoneNumber() {
 
         String u1FirstName = "Donald";
@@ -104,6 +128,9 @@ public class UserTest extends ActivityInstrumentationTestCase2 {
         assertTrue("Phone number not the same", u1PhoneNumber.equals(u1.getPhoneNumber()));
     }
 
+    /**
+     * Test get email.
+     */
     public void testGetEmail() {
 
         String u1FirstName = "Donald";
