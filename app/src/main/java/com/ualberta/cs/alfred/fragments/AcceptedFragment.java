0862e712b03861618ef13bb5d37d208c3808bb20
ccastell <ccastell@ualberta.ca>
2016-11-12 00:49:34 -0700

    tried to implement a listview

--- app/src/main/java/com/ualberta/cs/alfred/fragments/AcceptedFragment.java
+++ app/src/main/java/com/ualberta/cs/alfred/fragments/AcceptedFragment.java
@@ -39,6 +39,8 @@ public class AcceptedFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_accepted,container,false);
+
+
         return view;
     }
 
