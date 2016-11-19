abc98986f28459a7d0f1db85f61609149f6b1331
ookmm <gaylordmbuyi@gmail.com>
2016-11-18 22:41:22 -0700

    Changed IP before pushing

--- app/src/main/java/com/ualberta/cs/alfred/ESSettings.java
+++ app/src/main/java/com/ualberta/cs/alfred/ESSettings.java
@@ -12,7 +12,7 @@ import com.searchly.jestdroid.JestDroidClient;
  */
 public class ESSettings {
 
-    private static final String SERVER_URI = "http://192.168.1.213:9200";
+    private static final String SERVER_URI = "http://ela1.ookoo.co:9200";
     static final String INDEX_NAME = "alfred";
     static final String REQUEST_TYPE_NAME = "request";
     static final String RIDER_TYPE_NAME = "rider";
