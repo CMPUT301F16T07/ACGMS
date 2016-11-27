00c22a1ce71b13812db9ceb2fbe3e479ee1ae569
ookmm <gaylordmbuyi@gmail.com>
2016-11-27 15:03:57 -0700

    Test done for: As a rider, I want an estimate of a fair fare to offer to drivers.

--- app/src/androidTest/java/com/ualberta/cs/alfred/usecasetests/RequestsTest.java
+++ app/src/androidTest/java/com/ualberta/cs/alfred/usecasetests/RequestsTest.java
@@ -348,4 +348,138 @@ public class RequestsTest {
 
     }
 
+    /**
+     * US 01.05.01
+     * As a rider, I want to be able to phone or email the driver who accepted a request.
+     */
+    // TODO: Micheal's
+
+    /**
+     * US 01.06.01
+     * As a rider, I want an estimate of a fair fare to offer to drivers.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testGetFareEstimate() throws Exception {
+        /**
+         * Step 1: Basic user info
+         */
+        String u1FirstName = "Stephen";
+        String u1LastName = "Harper";
+        String u1UserName = "sharper";
+
+        // Create date
+        GregorianCalendar gc = new GregorianCalendar(1962, Calendar.FEBRUARY, 17);
+        Date u1DateOfBirth = gc.getTime();
+
+        String u1PhoneNumber = "780-888-5555";
+        String u1Email = "sharper@example.com";
+
+        /**
+         * Step 2: Create credit card
+         */
+        String creditCard = "2222333366662222";
+
+        /**
+         * Step 3: Create Rider info
+         */
+        RiderInfo riderInfo = new RiderInfo(creditCard);
+
+        /**
+         * Step 4: Check if DB has username already
+         */
+        UserESGetController.GetUserTask retrievedUser =
+                new UserESGetController.GetUserTask();
+
+        // Find user
+        retrievedUser.execute(u1UserName);
+
+        try {
+            User user = retrievedUser.get();
+
+            if (user == null) {
+                /**
+                 * Step 5: Create user
+                 */
+                User u1 = new User(u1FirstName, u1LastName, u1UserName, u1DateOfBirth, u1PhoneNumber,
+                        u1Email, riderInfo);
+            } else {
+                Log.i("Error", "Try a different username");
+                System.out.println("Try a different username");
+            }
+            assert (true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * Step 6: Get user ID with the initial username
+         */
+        String user2ID = null;
+
+        UserESGetController.GetUserTask retrievedUserForUserID =
+                new UserESGetController.GetUserTask();
+
+        // Find user
+        retrievedUserForUserID.execute(u1UserName);
+
+        try {
+            User user2 = retrievedUserForUserID.get();
+            if (user2 != null) {
+                user2ID = user2.getUserID();
+            }
+            assert (true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * Step 7: Make a request between two locations
+         */
+        String req1Status = AppSettings.REQUEST_PENDING;
+        Address req1SrcAddr = new Address("University of Alberta", -113.52631859999997, 53.5232189);
+        Address req1DestAddr = new Address("West Edmonton Mall", -113.62419060000002, 53.5225151);
+        double req1Cost = 16.65;
+        double req1Distance = 10.4;
+        String req1RiderID = user2ID;
+
+        Request req1 = new Request(req1Status, req1SrcAddr, req1DestAddr, req1Distance, req1Cost,
+                req1RiderID);
+        assert(true);
+
+        /**
+         * Step 8: Get requests by user with riderID
+         */
+        RequestESGetController.GetRequestTask retrievedRequest =
+                new RequestESGetController.GetRequestTask();
+
+        retrievedRequest.execute("riderID", user2ID);
+
+        // Get the ID of the request to get fare estimate.
+        // In this case, any request would do. We have chosen the first request that comes back
+        // from Elasticsearch.
+
+        try {
+            ArrayList<Request> requests = retrievedRequest.get();
+            if (requests.size() != 0) {
+                Request requestToGetFareEstimate = requests.get(0);
+                System.out.println("==================================");
+                System.out.println("Fare estimate is: " + requestToGetFareEstimate.getCost());
+                System.out.println("==================================");
+            } else {
+                Log.i("Error", "No request exists for this user");
+                System.out.println("No request exists for this user");
+            }
+            assert (true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
 }
