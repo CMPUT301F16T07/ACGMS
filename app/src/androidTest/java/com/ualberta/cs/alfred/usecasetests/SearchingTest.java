a56410c6b868c26faf9591ae36a9541ae6422df3
ookmm <gaylordmbuyi@gmail.com>
2016-11-27 23:33:50 -0700

    Test done for: As a driver, I want to browse and search for open requests by keyword.

--- app/src/androidTest/java/com/ualberta/cs/alfred/usecasetests/SearchingTest.java
+++ app/src/androidTest/java/com/ualberta/cs/alfred/usecasetests/SearchingTest.java
@@ -2,6 +2,7 @@ package com.ualberta.cs.alfred.usecasetests;
 
 import android.support.test.runner.AndroidJUnit4;
 
+import com.ualberta.cs.alfred.AppSettings;
 import com.ualberta.cs.alfred.Request;
 import com.ualberta.cs.alfred.RequestESGetController;
 
@@ -43,10 +44,47 @@ public class SearchingTest {
 
         // Get all requests within the specified distance
         retrievedRequest.execute(
-                "match_all", "all", "{}",
+                "requestStatus", "string", AppSettings.REQUEST_REQUESTED,
                 distance, coordinates
         );
 
+        try {
+            ArrayList<Request> requests = retrievedRequest.get();
+            for (Request request : requests) {
+                System.out.println("====================");
+                System.out.println("Request ID is: " + request.getRequestID());
+                System.out.println("Rider ID is: " + request.getRiderID());
+                System.out.println("Request Distance: " + request.getDistance());
+                System.out.println("Request Cost: " + request.getCost());
+                System.out.println("Request Status: " + request.getRequestStatus());
+                System.out.println("====================");
+
+                assert (true);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * US 04.02.01
+     * As a driver, I want to browse and search for open requests by keyword.
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testBrowseAndSearchByKeyword() throws Exception {
+        RequestESGetController.GetRequestByMultiplePreferencesTask retrievedRequest =
+                new RequestESGetController.GetRequestByMultiplePreferencesTask();
+
+        // Get open requests by keyword
+        String keyword = "Edmonton";
+        retrievedRequest.execute(
+                "requestStatus", "string", AppSettings.REQUEST_REQUESTED,
+                "_all", "string", keyword
+        );
 
         try {
             ArrayList<Request> requests = retrievedRequest.get();
