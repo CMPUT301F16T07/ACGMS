dd03a8e41b99a09e7a318fb201c1d8d24f41a4dc
ookmm <gaylordmbuyi@gmail.com>
2016-11-27 18:31:30 -0700

    Test done for: As a rider, I want to see some summary rating of the drivers who accepted my offers.

--- app/src/androidTest/java/com/ualberta/cs/alfred/usecasetests/RequestsTest.java
+++ app/src/androidTest/java/com/ualberta/cs/alfred/usecasetests/RequestsTest.java
@@ -733,7 +733,39 @@ public class RequestsTest {
         }
 
         /**
-         * Step 15: See driver's vehicle description
+         * Step 15: Get list of driver who accepted my request
+         */
+        RequestESGetController.GetRequestTask retrievedRequest2 =
+                new RequestESGetController.GetRequestTask();
+
+        retrievedRequest2.execute("riderID", user2ID);
+
+        // Get the ID of the request to add to driver's list.
+        // In this case, any request would do. We have chosen the first request that comes back
+        // from Elasticsearch.
+        Request riderRequest2 = null;
+        String riderRequestID2 = "";
+        ArrayList<String> myDriverIDList = null;
+
+        try {
+            ArrayList<Request> requests2 = retrievedRequest2.get();
+            if (requests2.size() != 0) {
+                riderRequest2 = requests2.get(0);
+                riderRequestID2 = riderRequest2.getRequestID();
+                myDriverIDList = riderRequest2.getDriverIDList();
+            } else {
+                Log.i("Error", "No request exists for this user");
+                System.out.println("No request exists for this user");
+            }
+            assert (true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * Step 16: See driver's vehicle description
          */
         if (driver2.getIsDriver()) {
             System.out.println("=============== VEHICLE INFO =================");
@@ -745,5 +777,297 @@ public class RequestsTest {
             System.out.println("=========== END OF VEHICLE INFO ==============");
         }
 
+    }
+
+    /**
+     * US 1.10.01 (added 2016-11-14)
+     * As a rider, I want to see some summary rating of the drivers who accepted my offers.
+     */
+    @Test
+    public void testSeeDriverSummaryRating() throws Exception {
+        /**
+         * Step 1: Basic driver info
+         */
+        String driverFirstName = "Eddie";
+        String driverLastName = "Santos";
+        String driverUserName = "esantos";
+
+        // Create date
+        GregorianCalendar gcDriver = new GregorianCalendar(1988, Calendar.JANUARY, 13);
+        Date driverDateOfBirth = gcDriver.getTime();
+
+        String driverPhoneNumber = "780-333-5555";
+        String driverEmail = "esantos@example.com";
+
+        /**
+         * Step 2: Create a driver licenceNumber
+         */
+        String licenceNumber = "ABB005";
+
+        /**
+         * Step 3: Create a vehicle
+         */
+        String serialNumber = "serial990088";
+        String plateNumber = "HELLO2";
+        String type = "Truck";
+        String make = "Toyota";
+        String model = "Tacoma";
+        int year = 2016;
+        String color = "Blue";
+
+        Vehicle vehicle = new Vehicle(serialNumber, plateNumber, type, make, model, year, color);
+
+        /**
+         * Step 4: Create driverInfo
+         */
+        DriverInfo driverInfo = new DriverInfo(licenceNumber, vehicle);
+
+        /**
+         * Step 5: Create driver if doesn't exist
+         */
+        UserESGetController.GetUserTask retrievedDriver =
+                new UserESGetController.GetUserTask();
+
+        // Find driver
+        retrievedDriver.execute(driverUserName);
+
+        try {
+            User user = retrievedDriver.get();
+
+            if (user == null) {
+                // Create driver
+                User driver = new User(driverFirstName, driverLastName, driverUserName,
+                        driverDateOfBirth, driverPhoneNumber, driverEmail, driverInfo);
+            } else {
+                Log.i("Error", "User already registered. Try a different username");
+                System.out.println("User already registered. Try a different username");
+            }
+            assert (true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * Step 6: Get driver user ID with the initial username
+         */
+        String driverID = null;
+
+        UserESGetController.GetUserTask retrievedDriverForUserID =
+                new UserESGetController.GetUserTask();
+
+        // Find driver
+        retrievedDriverForUserID.execute(driverUserName);
+
+        User driver2 = null;
+
+        try {
+            driver2 = retrievedDriverForUserID.get();
+            if (driver2 != null) {
+                driverID = driver2.getUserID();
+            }
+            assert (true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * Step 7: Basic rider info
+         */
+        String u1FirstName = "Stephen";
+        String u1LastName = "Harper";
+        String u1UserName = "sharper";
+
+        // Create date
+        GregorianCalendar gc = new GregorianCalendar(1962, Calendar.FEBRUARY, 17);
+        Date u1DateOfBirth = gc.getTime();
+
+        String u1PhoneNumber = "780-888-5555";
+        String u1Email = "sharper@example.com";
+
+        /**
+         * Step 8: Create credit card
+         */
+        String creditCard = "2222333366662222";
+
+        /**
+         * Step 9: Create Rider info
+         */
+        RiderInfo riderInfo = new RiderInfo(creditCard);
+
+        /**
+         * Step 10: Create rider if doesn't exist
+         */
+        UserESGetController.GetUserTask retrievedUser =
+                new UserESGetController.GetUserTask();
+
+        // Find user
+        retrievedUser.execute(u1UserName);
+
+        try {
+            User user = retrievedUser.get();
+
+            if (user == null) {
+                // Create user
+                User u1 = new User(u1FirstName, u1LastName, u1UserName, u1DateOfBirth, u1PhoneNumber,
+                        u1Email, riderInfo);
+            } else {
+                Log.i("Error", "Try a different username");
+                System.out.println("Try a different username");
+            }
+            assert (true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * Step 11: Get user ID with the initial username
+         */
+        String user2ID = null;
+
+        UserESGetController.GetUserTask retrievedUserForUserID =
+                new UserESGetController.GetUserTask();
+
+        // Find user
+        retrievedUserForUserID.execute(u1UserName);
+
+        try {
+            User user2 = retrievedUserForUserID.get();
+            if (user2 != null) {
+                user2ID = user2.getUserID();
+            }
+            assert (true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * Step 12: Make a request between two locations
+         */
+        String req1Status = AppSettings.REQUEST_PENDING;
+        Address req1SrcAddr = new Address("1978 99 St NW, Edmonton", -113.483609, 53.44708019999999);
+        Address req1DestAddr = new Address("11211 142 St NW, Edmonton", -113.5640156, 53.5612133);
+        double req1Cost = 19.56;
+        double req1Distance = 20.2;
+        String req1RiderID = user2ID;
+
+        Request req1 = new Request(req1Status, req1SrcAddr, req1DestAddr, req1Distance, req1Cost,
+                req1RiderID);
+        assert(true);
+
+        /**
+         * Step 13: Get requests by user with riderID
+         */
+        RequestESGetController.GetRequestTask retrievedRequest =
+                new RequestESGetController.GetRequestTask();
+
+        retrievedRequest.execute("riderID", user2ID);
+
+        // Get the ID of the request to add to driver's list.
+        // In this case, any request would do. We have chosen the first request that comes back
+        // from Elasticsearch.
+        Request riderRequest = null;
+        String riderRequestID = "";
+
+        try {
+            ArrayList<Request> requests = retrievedRequest.get();
+            if (requests.size() != 0) {
+                riderRequest = requests.get(0);
+                riderRequestID = riderRequest.getRequestID();
+            } else {
+                Log.i("Error", "No request exists for this user");
+                System.out.println("No request exists for this user");
+            }
+            assert (true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * Step 14: Add driver to driverIDList
+         */
+        if (!(riderRequestID.isEmpty())) {
+            String requestProperty = "driverIDList";
+            String requestPropertyType = "array";
+            String requestNewValue = driverID;
+
+            RequestESSetController.SetPropertyValueTask setPropertyValueTask =
+                    new RequestESSetController.SetPropertyValueTask();
+
+            setPropertyValueTask.execute(riderRequestID, requestProperty, requestPropertyType,
+                    requestNewValue);
+            assert (true);
+        } else {
+            Log.i("Error", "Request ID is empty");
+            System.out.println("Request ID is empty");
+        }
+
+        /**
+         * Step 15: Get list of driver who accepted my request
+         */
+        RequestESGetController.GetRequestTask retrievedRequest2 =
+                new RequestESGetController.GetRequestTask();
+
+        retrievedRequest2.execute("riderID", user2ID);
+
+        // Get the ID of the request to add to driver's list.
+        // In this case, any request would do. We have chosen the first request that comes back
+        // from Elasticsearch.
+        Request riderRequest2 = null;
+        String riderRequestID2 = "";
+        ArrayList<String> myDriverIDList = null;
+
+        try {
+            ArrayList<Request> requests2 = retrievedRequest2.get();
+            if (requests2.size() != 0) {
+                riderRequest2 = requests2.get(0);
+                riderRequestID2 = riderRequest2.getRequestID();
+                myDriverIDList = riderRequest2.getDriverIDList();
+            } else {
+                Log.i("Error", "No request exists for this user");
+                System.out.println("No request exists for this user");
+            }
+            assert (true);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        /**
+         * Step 16: Get every ride inside the driverIDList
+         */
+        for (int i = 0; i < myDriverIDList.size(); ++i) {
+            UserESGetController.GetUserByIdTask retrievedDriverFromList =
+                    new UserESGetController.GetUserByIdTask();
+
+            // Find driver
+            retrievedDriverFromList.execute(myDriverIDList.get(i));
+
+            try {
+                User driver = retrievedDriverFromList.get();
+
+                System.out.println("====================");
+                System.out.println("Username: " + driver.getUserName());
+                System.out.println("First name: " + driver.getFirstName());
+                System.out.println("Last name: " + driver.getLastName());
+                System.out.println("Rating: " + driver.getDriverInfo().getDriverRating().getRating());
+                System.out.println("====================");
+                assert (true);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
